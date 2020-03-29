189e0217e5f8945aa61640789540c151cc75c8fa
Romain Rouvoy <romain@rouvoy.fr>
2020-03-29 18:25:47 +0200

    doc: Adding Licence headers

--- pyworkload/__main__.py
+++ pyworkload/__main__.py
@@ -1,20 +1,33 @@
+
+# Copyright (c) 2020, Romain Rouvoy
+# 
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+# 
+#     * Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright notice,
+#       this list of conditions and the following disclaimer in the documentation
+#       and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
+# OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 from .core import Workload
-from .backend import DockerBackend
 from .configuration import WorkloadCLI
 
-# import argparse
 
-# parser = argparse.ArgumentParser(description='Starts several containers in parallel.')
-
-# parser.add_argument('input', type=str, help='commands file (.yaml)')
-# parser.add_argument("-m", "--mean", type=int, default=2,
-#                     help="mean number of containers")
-# parser.add_argument("-s", "--seconds", type=int, default=30,
-#                     help="interval in seconds")
-# parser.add_argument("-t", "--times", type=int, default=10,
-#                     help="number of times")
-# parser.add_argument("--pull", action="store_true", help="Only pulls the images")
-# args = parser.parse_args()
 cli = WorkloadCLI()
 configuration = cli.generate_configuration()
 
